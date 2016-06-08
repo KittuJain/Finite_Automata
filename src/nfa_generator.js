e19a59d4b3d228c7d0bef206b5b5e59d21f95306
KratiJain <cskratijain@gmail.com>
2016-06-08 14:50:26 +0530

    Handling epsilon in NFA-Generator

--- src/nfa_generator.js
+++ src/nfa_generator.js
@@ -1,13 +1,27 @@
 var _ = require('lodash');
 
 exports.nfa_generator = function (language) {
-  return function (inputString) {
-      var alphabets = inputString.split('');
-      var outputStates = alphabets.reduce(function (states, currentAlphabet) {
-          return _.flatten(states.map(function (state) {
-              return language.transitionFunc[state][currentAlphabet] || [];
-          }));
-      }, [language.initialState]);
-      return _.intersection(language.finalStates, outputStates).length > 0;
-  }
+    return function (inputString) {
+        var outputStates = stateReducer(inputString, language.transitionFunc, language.initialState);
+        return _.intersection(language.finalStates, outputStates).length > 0;
+    }
+};
+
+var stateReducer = function (inputString, transitions, initialState) {
+    return inputString.split('').reduce(function (states, currentAlphabet) {
+        return stateMapper(states, transitions, currentAlphabet);
+    }, [initialState]);
+};
+
+var stateReducerOnEpsilon = function (transitions, state, currentAlphabet) {
+    return stateMapper(transitions[state]['ε'], transitions, currentAlphabet)
+};
+
+var stateMapper = function (states, transitions, currentAlphabet) {
+    return _.flatten(states.map(function (state) {
+        if (_.includes(Object.keys(transitions[state]), 'ε')) {
+            return stateReducerOnEpsilon(transitions, state, currentAlphabet);
+        }
+        return transitions[state][currentAlphabet] || []
+    }));
 };
--- test/nfa_generator_test.js
+++ test/nfa_generator_test.js
@@ -22,22 +22,24 @@ describe('nfa-test', function () {
             assert.isTrue(nfa("1101"));
         });
 
-        it('strings ending with exactly two 1s or even number of 1s', function () {
+        it('strings ending with an even number of 1s or even number of 0s', function () {
             var language = {
-                states: ["q1", "q2", "q3", "q4", "q5", "q6"],
-                alphabets: ["1", "0", "E"],
+                states: ["q1", "q2", "q3", "q4", "q5"],
+                alphabets: ["1", "0", "ε"],
                 transitionFunc: {
-                    "q1": {"1": ["q2", "q4"], "E": ["q4"]},
-                    "q2": {"1": ["q3"]},
-                    "q4": {"0": ["q5"]},
-                    "q5": {"0": ["q6"]}
+                    "q1": {"ε": ["q2", "q4"]},
+                    "q2": {"0": ["q3"], "1": ["q2"]},
+                    "q3": {"0": ["q2"], "1": ["q3"]},
+                    "q4": {"0": ["q4"], "1": ["q5"]},
+                    "q5": {"0": ["q5"], "1": ["q4"]}
                 },
                 initialState: "q1",
-                finalStates: ["q3", "q6"]
+                finalStates: ["q2", "q4"]
             };
             var nfa = nfa_generator(language);
-            assert.isFalse(nfa("01"));
             assert.isTrue(nfa("11"));
+            assert.isTrue(nfa("00"));
+            assert.isFalse(nfa("01"));
         });
 
     });
