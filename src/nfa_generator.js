67376acaeb85ff7b8c17eab93b8eeea5177ad3ee
KratiJain <cskratijain@gmail.com>
2016-06-09 12:14:17 +0530

    Reusing stateMapper instead of specifically checking for epsilons at end separately

--- src/nfa_generator.js
+++ src/nfa_generator.js
@@ -4,15 +4,12 @@ var epsilon = 'e';
 exports.nfa_generator = function (language) {
     return function (inputString) {
         var reducedStates = stateReducer(inputString, language.transitionFunc, language.initialState);
-        var outputStates = mapForEpsilonAtEnd(reducedStates, language.transitionFunc, null);
+        var outputStates = stateMapper(reducedStates, language.transitionFunc, null);
         return _.intersection(language.finalStates, outputStates).length > 0;
     }
 };
 
 var stateReducer = function (inputString, transitions, initialState) {
-    var transitionOnEpsilon = transitions[initialState][epsilon];
-    if (inputString.length == 0)
-        return (transitionOnEpsilon ? transitionOnEpsilon : [initialState]);
     return inputString.split('').reduce(function (states, currentAlphabet) {
         return stateMapper(states, transitions, currentAlphabet);
     }, [initialState]);
@@ -25,19 +22,10 @@ var stateMapper = function (states, transitions, currentAlphabet) {
         if (containsEpsilon(transitions, state)) {
             return stateOnCurrentAlphabet.concat(stateMapper(transitions[state][epsilon], transitions, currentAlphabet));
         }
-        return transitions[state] && stateOnCurrentAlphabet || states;
+        return transitions[state] && currentAlphabet && stateOnCurrentAlphabet || states;
     }));
 };
 
 var containsEpsilon = function (transitions, state) {
     return (!_.isEmpty(Object.keys(transitions[state])) && _.includes(Object.keys(transitions[state]), epsilon));
-};
-
-var mapForEpsilonAtEnd = function (states, transitions) {
-    return _.flatten(states.map(function (state) {
-        if (containsEpsilon(transitions, state))
-            return transitions[state][epsilon];
-        else
-            return states;
-    }));
 };
